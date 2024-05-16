c4274a7e7e99a4499d5b3e38e20fcbce52a70245
Akshay Rajan <akshayrajan345@gmail.com>
2024-05-16 22:32:07 +0530

    Algorithm Completed

--- OpenCourseAllotment/allotment_algorithm.py
+++ OpenCourseAllotment/allotment_algorithm.py
@@ -1,30 +1,48 @@
 import csv
 
-classes = {
-    "Malayalam": 4,
-    "English": 4,
-    "Hindi": 4,
-    "Tamil": 4,
-    "Physics": 4,
-    "Chemistry": 4,
-    "Botany": 4,
-    "Zoology": 4,
-    "Mathematics": 4,
-    "Economics": 4,
-    "History": 4,
-    "Philosophy": 4
-}
-
 def allotStudents(file):
     """Input a csv of all students and their choices, allots them to classes"""
     
+    classes = {
+        "Malayalam": 4,
+        "English": 4,
+        "Hindi": 4,
+        "Tamil": 4,
+        "Physics": 4,
+        "Chemistry": 4,
+        "Botany": 4,
+        "Zoology": 4,
+        "Mathematics": 4,
+        "Economics": 4,
+        "History": 4,
+        "Philosophy": 4
+    }
+    # Read the file and store the data as a list of dictionaries
+    students = []
     with open(file, "r") as f:
         reader = csv.reader(f)
         for row in reader:
-            print(row)
+            name, admn_no, percentage, c1, c2, c3, c4, c5 = row[0], row[1], row[2], row[3].strip(), row[4].strip(), row[5].strip(), row[6].strip(), row[7].strip()
+            student  = {"name": name, "admn_no": admn_no, "percentage": percentage, "choices": [c1, c2, c3, c4, c5]}
+            students.append(student)
+
+    # Sort the students according to their percentage
+    students.sort(key=lambda x: x["percentage"], reverse = True)
     
-    return {}
+    # Allot the students to their choices
+    allotment = {}
+    for student in students:
+        for choice in student["choices"]:
+            if classes[choice] > 0:
+                classes[choice] -= 1
+                print(f"{student['name']} ({student['admn_no']}) allotted to {choice}")
+                allotment[choice] = allotment.get(choice, []) + [(student['name'], student['admn_no'])]
+                break
+            else:
+                print(f"{student['name']} ({student['admn_no']}) not allotted to {choice}")
+        
+    return allotment
             
 
 if __name__ == "__main__":
-    allotStudents("file.txt")
+    print(allotStudents("file.txt"))
