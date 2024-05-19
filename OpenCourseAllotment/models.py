7730e7c87ebfec2097d414bdcf2f3726bcad8c05
Akshay Rajan <akshayrajan345@gmail.com>
2024-05-19 17:16:32 +0530

    Update Models

--- OpenCourseAllotment/models.py
+++ OpenCourseAllotment/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """Custom user model"""
+    
+    is_teacher = models.BooleanField(default=False)
+    # USERNAME_FIELD = 'username  '
+    # REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.username
