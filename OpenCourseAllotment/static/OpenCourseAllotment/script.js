be0a86d36009d89bf4d44dbed6e98b95d0e4d064
Akshay Rajan <akshayrajan345@gmail.com>
2024-05-22 23:45:27 +0530

    Enable register toggle buttons

--- OpenCourseAllotment/static/OpenCourseAllotment/script.js
+++ OpenCourseAllotment/static/OpenCourseAllotment/script.js
@@ -1,1 +1,25 @@
-console.log(1);
+document.addEventListener('DOMContentLoaded', ()=> {
+    toggleLogin();
+});
+
+// ! Login Toggler
+function toggleLogin() {
+    let buttons = document.querySelectorAll('.reg-buttons button');
+    let trReg = document.querySelector('.tr-reg');
+    let studReg = document.querySelector('.stud-reg');
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Use the 'data' attribute to find which button has been clicked
+            if (this.dataset.role == "teacher") {
+                trReg.style.display = 'block';
+                studReg.style.display = 'none';
+                console.log('a');
+            } else {
+                trReg.style.display = 'none';
+                studReg.style.display = 'block';
+                console.log('b');
+            }
+        });
+    });
+}
+
